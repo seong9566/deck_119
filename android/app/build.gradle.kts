c6f75a3b529baf1e9dc304cbd04aaeeca07b81e9
seong9566 <seong9566@users.noreply.github.com>
2026-07-12 00:54:01 +0900

    chore(android/ios): 패키지명 변경 + Firebase 초기 설정(google-services, BoM)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     id("com.android.application")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.deck_119"
+    namespace = "com.seong.deck119"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +20,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.deck_119"
+        applicationId = "com.seong.deck119"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -42,4 +46,16 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
--- android/app/src/main/kotlin/com/seong/deck119/MainActivity.kt
+++ android/app/src/main/kotlin/com/seong/deck119/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.deck_119
+package com.seong.deck119
 
 import io.flutter.embedding.android.FlutterActivity
 
